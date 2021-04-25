340ebabce2f07233ac603f21671a6293c0acc4e2
1906miral <barhoumimiral88@gmail.com>
2021-04-25 21:53:27 +0100

    integration1.1

---
+++ PiDevWeb2.4/src/Entity/Activite.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Activite
+ *
+ * @ORM\Table(name="activite", indexes={@ORM\Index(name="fk_idcoach_act", columns={"id_coachact"}), @ORM\Index(name="fk_idcat", columns={"id_categorie"})})
+ * @ORM\Entity
+ */
+class Activite
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id_activite", type="integer", nullable=false)
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $idActivite;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="titre", type="string", length=20, nullable=false)
+     */
+    private $titre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=100, nullable=false)
+     */
+    private $description;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nomcat", type="string", length=20, nullable=false)
+     */
+    private $nomcat;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="date", type="date", nullable=false)
+     */
+    private $date;
+
+    /**
+     * @var \Coach
+     *
+     * @ORM\ManyToOne(targetEntity="Coach")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_coachact", referencedColumnName="id_coach")
+     * })
+     */
+    private $idCoachact;
+
+    /**
+     * @var \Categorie
+     *
+     * @ORM\ManyToOne(targetEntity="Categorie")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="id_categorie", referencedColumnName="id_categorie")
+     * })
+     */
+    private $idCategorie;
+
+
+}
