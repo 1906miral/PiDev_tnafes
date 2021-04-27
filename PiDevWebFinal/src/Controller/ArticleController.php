74cb04deaf8357f9258fcd141b727f824ed28ba7
1906miral <barhoumimiral88@gmail.com>
2021-04-28 00:47:24 +0100

    final symfony project

---
+++ PiDevWebFinal/src/Controller/ArticleController.php
@@ -0,0 +1,126 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use App\Form\ArticleType;
+use App\Repository\ArticleRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+//use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Serializer\Normalizer\NormalizerInterface;
+
+/**
+ * @Route("/article")
+ */
+class ArticleController extends AbstractController
+{
+    /**
+     * @Route("/", name="article_index", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $articles = $this->getDoctrine()
+            ->getRepository(Article::class)
+            ->findAll();
+
+        return $this->render('article/index.html.twig', [
+            'articles' => $articles,
+        ]);
+    }
+
+
+
+    /**
+     * @Route("/new", name="article_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $article = new Article();
+        $article->setDate(new \DateTime('now'));
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($article);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('article_index');
+        }
+
+        return $this->render('article/new.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{idArticle}", name="article_show", methods={"GET"})
+     */
+    public function show(Article $article): Response
+    {
+        return $this->render('article/show.html.twig', [
+            'article' => $article,
+        ]);
+    }
+
+
+
+
+
+    /**
+     * @Route("/{idArticle}/edit", name="article_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Article $article): Response
+    {
+        $form = $this->createForm(ArticleType::class, $article);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('article_index');
+        }
+
+        return $this->render('article/edit.html.twig', [
+            'article' => $article,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{idArticle}", name="article_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Article $article): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$article->getIdArticle(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($article);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('article_index');
+    }
+
+
+
+    /**
+     * @Route("/s/searchArticlex", name="searchArticlex")
+     */
+    public function searchArticle(Request $request,NormalizerInterface $Normalizer,ArticleRepository $repository):Response
+    {
+//$repository = $this->getDoctrine()->getRepository(Article::class);
+$requestString=$request->get('searchValue');
+$articles = $repository->findpartitre($requestString);
+$jsonContent = $Normalizer->normalize($articles, 'json',['Groups'=>'articles:read']);
+$retour =json_encode($jsonContent);
+
+return new Response($retour);
+
+        }
+
+
+}
